bd672a80dedae65dfb7a5c6f3b7db9452eb4ef50
Matheus Rocha <matheus_rocha98@outlook.com>
2020-10-06 09:26:27 -0300

    fix start and db name

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import connection from './database/connection'
 
-connection('alunos').query('SELECT 1+? as test1', [10], (err, rows) => {
+connection('bdd').query('SELECT 1+? as test1', [10], (err, rows) => {
   if (err) console.log(err)
   else console.log(rows)
 })
